6e88198cd67a5f465eae718611da7064088ca1ef
danWagar <dandwagar@gmail.com>
2019-11-01 10:09:48 -0700

    Revert "heroku"
    This reverts commit 88cc249674e9777dcc0ab5ebd8f4ab243a690642.

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
-
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
-
-app.use(morgan(morganSetting));
+app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 require('dotenv').config();
@@ -52,17 +49,8 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get('/movie', handleGetMovie);
 
-const PORT = process.env.PORT || 8000;
-
-app.use((error, req, res, next) => {
-  let response;
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }};
-  } else {
-    response = { error };
-  }
-  res.status(500).json(response);
-});
+const PORT = 8000;
 
 app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
